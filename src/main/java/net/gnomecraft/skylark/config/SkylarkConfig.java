57575f07f7c59cfdc4f1db5b665a0856a41c2310
gniftygnome <minecraft@gnifty.net>
2022-05-25 18:05:17 -0700

    Allow randomized spawn features from configured list.

--- src/main/java/net/gnomecraft/skylark/config/SkylarkConfig.java
+++ src/main/java/net/gnomecraft/skylark/config/SkylarkConfig.java
@@ -4,14 +4,33 @@ import blue.endless.jankson.Comment;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import net.gnomecraft.skylark.Skylark;
-import net.minecraft.util.Identifier;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // Configuration file definition.
 @SuppressWarnings("unused")
 @Config(name = Skylark.modId)
 public class SkylarkConfig implements ConfigData {
-    @Comment("What do you want as a spawn platform?")
-    public Identifier spawnPlatform = new Identifier("minecraft:mega_spruce");
+    @Comment("What do you want as a spawn platform (randomly selected from list)?")
+    public ArrayList<String> spawnPlatform = new ArrayList<>(List.of(
+            "acacia",
+            "azalea_tree",
+            "birch",
+            "dark_oak",
+            "fancy_oak",
+            "fancy_oak_bees",
+            "jungle_tree",
+            "mangrove",
+            "tall_mangrove",
+            "mega_jungle_tree",
+            "mega_pine",
+            "mega_spruce",
+            "oak",
+            "pine",
+            "spruce",
+            "super_birch_bees"
+    ));
 
     @Comment("Space teams out around the origin at what radius?  (Zero for shared spawn point.)")
     public long spawnRingRadius = 0;
--- src/main/java/net/gnomecraft/skylark/spawn/SetupSpawnPoint.java
+++ src/main/java/net/gnomecraft/skylark/spawn/SetupSpawnPoint.java
@@ -12,28 +12,46 @@ import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.TreeConfiguredFeatures;
 
+import java.util.ArrayList;
+
 public class SetupSpawnPoint {
 
     public static void generatePlatform(ServerWorld world, BlockPos spawnPos, Chunk spawnChunk) {
         ChunkGenerator chunkGenerator = world.getChunkManager().getChunkGenerator();
-        Identifier configSpawnPlatform = Skylark.getConfig().spawnPlatform;
+        ArrayList<String> platformList = new ArrayList<>(Skylark.getConfig().spawnPlatform);
 
-        if (BuiltinRegistries.CONFIGURED_STRUCTURE_FEATURE.containsId(configSpawnPlatform)) {
-            Skylark.LOGGER.warn("Spawn platform structures are currently unsupported!");
-        }
-        if (Registry.BLOCK.getOrEmpty(configSpawnPlatform).isPresent()) {
-            Block spawnBlock = Registry.BLOCK.get(configSpawnPlatform);
-            world.setBlockState(spawnPos, spawnBlock.getDefaultState());
-            Skylark.LOGGER.info("Spawn platform from config is a block: " + spawnBlock);
-        } else {
-            ConfiguredFeature<?, ?> spawnFeature;
-            if ((spawnFeature = BuiltinRegistries.CONFIGURED_FEATURE.get(configSpawnPlatform)) != null) {
-                Skylark.LOGGER.info("Spawn platform from config is a feature: " + spawnFeature);
-            } else {
-                spawnFeature = TreeConfiguredFeatures.MEGA_SPRUCE.value();
-                Skylark.LOGGER.info("Spawn platform is default feature: " + spawnFeature);
+        while (platformList.size() > 0) {
+            // Yank a random ID from our copy of the config list
+            Identifier configSpawnPlatform = new Identifier(platformList.remove(world.getRandom().nextInt(platformList.size())));
+
+            // Block spawn platform
+            if (Registry.BLOCK.getOrEmpty(configSpawnPlatform).isPresent()) {
+                Block spawnBlock = Registry.BLOCK.get(configSpawnPlatform);
+                Skylark.LOGGER.info("Spawn platform from config is a block: " + spawnBlock);
+                world.setBlockState(spawnPos, spawnBlock.getDefaultState());
+                return;
             }
-            spawnFeature.generate(world, chunkGenerator, world.random, spawnPos);
+
+            // Structure spawn platform
+            if (BuiltinRegistries.CONFIGURED_STRUCTURE_FEATURE.containsId(configSpawnPlatform)) {
+                Skylark.LOGGER.warn("Spawn platform structures are currently unsupported!");
+            }
+
+            // Feature spawn platform
+            if (BuiltinRegistries.CONFIGURED_FEATURE.containsId(configSpawnPlatform)) {
+                ConfiguredFeature<?, ?> spawnFeature = BuiltinRegistries.CONFIGURED_FEATURE.get(configSpawnPlatform);
+                assert(spawnFeature != null);  // Why would somebody add a null feature to the registry?!
+                Skylark.LOGGER.info("Spawn platform from config is a feature: " + spawnFeature);
+                spawnFeature.generate(world, chunkGenerator, world.random, spawnPos);
+                return;
+            }
+
+            Skylark.LOGGER.warn("Cannot resolve spawn platform from config: " + configSpawnPlatform);
         }
+
+        // Default spawn platform
+        ConfiguredFeature<?, ?> spawnFeature = TreeConfiguredFeatures.MEGA_SPRUCE.value();
+        Skylark.LOGGER.info("Spawn platform is default feature: " + spawnFeature);
+        spawnFeature.generate(world, chunkGenerator, world.random, spawnPos);
     }
 }
