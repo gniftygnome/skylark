69e9a318e259617e28ef69291dd930aece233053
gniftygnome <minecraft@gnifty.net>
2024-11-25 23:56:41 -0800

    Another minor nitpick.

--- src/main/java/net/gnomecraft/skylark/SkylarkClient.java
+++ src/main/java/net/gnomecraft/skylark/SkylarkClient.java
@@ -1,8 +1,10 @@
 package net.gnomecraft.skylark;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
-@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+@Environment(EnvType.CLIENT)
 public class SkylarkClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
